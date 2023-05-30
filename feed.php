ccea55c41dd0d22cf13c1cf732d2cc61525de23a
SkiServ <skirata@skiserv.eu>
2023-05-30 17:21:02 +0200

    change date format to have complete year

--- feed.php
+++ feed.php
@@ -4,13 +4,13 @@ $API_BASE_URL = "https://metropole.rennes.fr/";
 $ITEMS_LIMIT = 20;
 
 $content = file_get_contents(
-    $API_BASE_URL."articles?maxResultsByPage=".$ITEMS_LIMIT,
+    $API_BASE_URL . "articles?maxResultsByPage=" . $ITEMS_LIMIT,
     false,
-    stream_context_create(array("ssl"=>array('cafile' => './cert.pem'))),
+    stream_context_create(array("ssl" => array('cafile' => './cert.pem'))),
 );
 
 
-if(!$content) {
+if (!$content) {
     http_response_code(404);
     print("Error");
     return;
@@ -24,19 +24,15 @@ print('<?xml version="1.0" encoding="UTF-8"?>');
         <title>Ici Rennes Off</title>
         <description>Flux non officiel d'Ici Rennes</description>
         <link>https://metropole.rennes.fr/</link>
-        <lastBuildDate><?php echo (new DateTime())->format(DateTime::RFC822) ?></lastBuildDate>
+        <lastBuildDate><?php echo (new DateTime())->format("r") ?></lastBuildDate>
 
-<?php
-foreach ($data["articles"] as $article) {
-?>
-        <item>
-            <title><?php echo $article["title"] ?></title>
-            <link><?php echo $article["url"] ?></link>
-            <pubDate><?php echo (new DateTime())->setTimestamp($article["date"])->format(DateTime::RFC822) ?></pubDate>
-            <description></description>
-        </item>
-<?php
-}
-?>
+        <?php foreach ($data["articles"] as $article) { ?>
+            <item>
+                <title><?php echo $article["title"] ?></title>
+                <link><?php echo $article["url"] ?></link>
+                <pubDate><?php echo (new DateTime())->setTimestamp($article["date"])->format("r") ?></pubDate>
+                <description></description>
+            </item>
+        <?php } ?>
     </channel>
 </rss>
